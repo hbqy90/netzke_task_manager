911648ed9b65d484cc4b24b9acbec92f0566a504
hbqy90 <hbqy90@gmail.com>
2015-03-12 23:56:42 +0800

    Tasks

---
+++ app/components/tasks.rb
@@ -0,0 +1,16 @@
+class Tasks < Netzke::Basepack::Grid
+	def configure(c)
+		
+		c.model = "Task"
+		#c.persistence = true
+		c.columns = [
+			:done,
+			:name,
+			{name: :notes, flex: 1},
+			:priority,
+			{name: :due, header: "Due on"}
+		]
+		c.scope = {done: [nil, false]}
+		super
+	end
+end
